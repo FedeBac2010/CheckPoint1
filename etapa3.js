10315a1f0f74fd7e11a8e5891055bc9d17ca7fa4
FedeBac2010 <fedebacigalupi2010@outlook.com>
2022-05-23 11:48:48 -0300

    Agregue la funcionalidad venderAuto por Patente

---
+++ etapa3.js
@@ -0,0 +1,27 @@
+let autos = require ('./autos.js');
+
+const concesionaria= {
+    autos: autos,
+
+    buscarAuto: function (patente){
+        autoBuscado= null;
+        this.autos.forEach( autos=>{
+        if (autos.patente=== patente){
+            autoBuscado= autos;
+        }})
+        return autoBuscado;
+    },
+
+    venderAuto: function (patente){
+        autosBuscado= this.buscarAuto(patente);
+        if (autoBuscado != null){
+            autoBuscado.vendido= true
+        }
+        return autoBuscado
+    }
+
+    }
+
+    console.log (concesionaria.venderAuto('APL123'))
+
+
