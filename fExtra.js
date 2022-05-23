48b4baa6c32bd6c375aa1c174e7abb2e6ea65e01
FedeBac2010 <fedebacigalupi2010@outlook.com>
2022-05-23 18:00:37 -0300

    Agregue la funcionalidad autosParaLaVenta

---
+++ fExtra.js
@@ -0,0 +1,32 @@
+let autos = require ('./autos.js');
+
+const concesionaria= {
+    autos: autos,
+
+    buscarAuto: function (patente){
+        autoBuscado= null;
+        this.autos.forEach( autos=>{
+        if (autos.patente=== patente){
+            autoBuscado= autos;
+        }})
+        return autoBuscado;
+    },
+
+    venderAuto: function (patente){
+        autosBuscado= this.buscarAuto(patente);
+        if (autoBuscado != null){
+            autoBuscado.vendido= true
+        }
+        return autoBuscado
+    },
+
+    autosParaLaVenta: function (){
+        lista= this.autos.filter (autos=> autos.vendido== false)
+        return lista
+    }
+
+    }
+
+    
+
+
