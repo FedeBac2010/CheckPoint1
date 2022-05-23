3b931c09e74b4bb038deb40521554c77547e05b9
FedeBac2010 <fedebacigalupi2010@outlook.com>
2022-05-23 18:31:14 -0300

    Agregue funcionliadad de listaDeVentas

---
+++ masFuncio2.js
@@ -0,0 +1,50 @@
+let autos = require ('./autos.js');
+
+const concesionaria= {
+    autos: autos,
+
+    buscarAuto: function (patente){
+        autoBuscado= null;
+        this.autos.forEach( autos=>{
+        if (autos.patente=== patente){
+            autoBuscado= autos;
+        }})
+        return autoBuscado;
+    },
+
+    venderAuto: function (patente){
+        autosBuscado= this.buscarAuto(patente);
+        if (autoBuscado != null){
+            autoBuscado.vendido= true
+        }
+        return autoBuscado
+    },
+
+    autosParaLaVenta: function (){
+        lista= this.autos.filter (autos=> autos.vendido== false)
+        return lista
+    },
+
+    autosNuevos: function (){
+        autosDisponibles= this.autosParaLaVenta();
+        autos0km= autosDisponibles.filter(auto0=> auto0.km <100)
+        return autos0km
+    },
+
+    listaDeVentas:function (){
+        autosVendidos= this.autos.filter(autos=> autos.vendido== true);
+        lista= [];
+        precios= autosVendidos.filter(autos=> lista.push(autos.precio))
+        return lista
+
+    }
+}
+
+
+
+
+
+
+    
+
+
